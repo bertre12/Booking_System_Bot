355a672a65bf5372d0035ac543d90cf15edf8d00
Leonid <Leonid@Leonid>
2025-02-24 13:25:37 +0300

    Создание run.py для запуска бота. Подключение токена бота(токен скрыт).

---
+++ run.py
@@ -0,0 +1,23 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from token_key import TOKEN
+
+
+# Запрос на сервер для получения ответа.
+async def main():
+    bot = Bot(token=TOKEN)  # Создание объекта бота и подключение его токена.
+    dp = Dispatcher()  # Создание диспетчера для управления ботом.
+    await bot.delete_webhook(drop_pending_updates=True)  # Игнорирование
+    # запросов при выключенном боте.
+    await dp.start_polling(bot)
+
+
+# Запуск бота.
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)  # Подключение логирования.
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print('Выход')
