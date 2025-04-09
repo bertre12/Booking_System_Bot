e4d22d70d71e304c7a1b16872cb8488c70510724
Leonid <Leonid@Leonid>
2025-04-09 19:55:28 +0300

    Добавление кода на создание таблицы users в бд PostgreSQL. Добавление функции для извлечения названия Гостиницы по её id. Добавление функции для записи данных в таблицу users.

--- database/database_postgresql.py
+++ database/database_postgresql.py
@@ -8,10 +8,10 @@ load_dotenv()
 # Данные для подключения к PostgreSQL на локальной машине.
 async def get_db_connection():
     return await asyncpg.connect(
-        user=os.getenv('POSTGRESQL_USER'),
-        password=os.getenv('POSTGRESQL_PASSWORD'),
-        database=os.getenv('POSTGRESQL_DATABASE'),
-        host=os.getenv('POSTGRESQL_HOST')
+        user=os.getenv("POSTGRESQL_USER"),
+        password=os.getenv("POSTGRESQL_PASSWORD"),
+        database=os.getenv("POSTGRESQL_DATABASE"),
+        host=os.getenv("POSTGRESQL_HOST"),
     )
 
 
@@ -21,18 +21,38 @@ async def db_setup():
     conn = await get_db_connection()
     try:
         # Создание таблицы hotel.
-        await conn.execute('''
+        await conn.execute(
+            """
             CREATE TABLE IF NOT EXISTS hotel (
                 id SERIAL PRIMARY KEY,
                 name VARCHAR(100) NOT NULL,
                 price DOUBLE PRECISION NOT NULL,
                 description TEXT
             )
-        ''')
+        """
+        )
+
+        # Создание таблицы users и связи 'один ко многим'.
+        await conn.execute(
+            """
+            CREATE TABLE IF NOT EXISTS users (
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(100) NOT NULL,
+                phone_number VARCHAR(20) NOT NULL,
+                email TEXT,
+                hotel_id INTEGER NOT NULL,
+                booking_number TEXT NOT NULL,
+                FOREIGN KEY (hotel_id) REFERENCES hotel(id) ON DELETE CASCADE
+            )
+        """
+        )
         print('База данных PostgreSQL подключена и таблицы созданы.')
     finally:
         # Закрытие соединения.
-        await conn.close()
+        try:
+            await conn.close()
+        except Exception as e:
+            print(f'Ошибка при закрытии соединения: {e}')
 
 
 # Функция для запроса к базе данных.
@@ -41,16 +61,30 @@ async def get_id_hotel_and_price_and_description_from_db():
     conn = await get_db_connection()
     try:
         # Выполнение запроса.
-        hotels = await conn.fetch('SELECT id, name, price, description FROM '
-                                  'hotel')
-        # Извлечение данных(гостиница и цена, описание) в виде списка.
-        return [{'id': hotel['id'], 'name': hotel['name'], 'price': hotel[
-            'price'],
-                 'description': hotel['description']} for hotel in
-                hotels]
+        hotels = await conn.fetch("SELECT id, name, price, description FROM " 
+                                  "hotel")
+        # Извлечение данных(id, гостиница, цена, описание) в виде списка.
+        return [
+            {
+                "id": hotel["id"],
+                "name": hotel["name"],
+                "price": hotel["price"],
+                "description": hotel["description"],
+            }
+            for hotel in hotels
+        ]
     finally:
         # Закрытие соединения.
         await conn.close()
+
+
+# Функция извлечения id названия гостиницы.
+async def get_hotel_id_from_db(hotel_name: str):
+    conn = await get_db_connection()
+    result = await conn.fetchrow("SELECT id FROM hotel WHERE name = $1",
+                                 hotel_name)
+    await conn.close()
+    return result["id"] if result else None
 
 
 # Функция извлечения названий гостиниц.
@@ -59,7 +93,7 @@ async def get_hotels_from_db():
     hotels_and_prices = await get_id_hotel_and_price_and_description_from_db()
 
     # Извлекаем названия гостиниц и сортируем их по алфавиту.
-    hotels = [hotel['name'] for hotel in hotels_and_prices]
+    hotels = [hotel["name"] for hotel in hotels_and_prices]
     hotels.sort()  # Сортировка по алфавиту.
 
     return hotels
@@ -75,4 +109,32 @@ async def get_descriptions_from_db():
     # Получаем данные из бд.
     hotels_and_descriptions = \
         await get_id_hotel_and_price_and_description_from_db()
-    return [hotel['description'] for hotel in hotels_and_descriptions]
+    return [hotel["description"] for hotel in hotels_and_descriptions]
+
+
+# Функция для записи данных в таблицу users.
+async def save_user(user_data):
+    # Получаем соединение с базой данных.
+    conn = await get_db_connection()
+    try:
+        await conn.execute(
+            """INSERT INTO users (
+            name, phone_number, email, hotel_id, booking_number
+            )
+            VALUES ($1, $2, $3, $4, $5)
+        """,
+            user_data["name"],
+            user_data["phone_number"],
+            user_data["email"],
+            user_data["hotel_id"],
+            user_data["booking_number"],
+        )
+        print('Данные успешно добавлены в таблицу users.')
+    except Exception as e:
+        print(f'Ошибка при добавлении данных: {e}')
+    finally:
+        # Закрываем соединение.
+        try:
+            await conn.close()
+        except Exception as e:
+            print(f'Ошибка при закрытии соединения: {e}')
